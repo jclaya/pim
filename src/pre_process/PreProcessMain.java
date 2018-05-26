1d6a89cc15b50afff4f7c6cf248a6fc93f87a7c8
jbclaya <jbclaya@gmail.com>
2018-05-26 21:42:58 +0800

    finished clahe and added multiple images name for output

--- src/pre_process/PreProcessMain.java
+++ src/pre_process/PreProcessMain.java
@@ -8,6 +8,7 @@ package pre_process;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.CLAHE;
 import org.opencv.imgproc.Imgproc;
 
 /**
@@ -17,14 +18,41 @@ import org.opencv.imgproc.Imgproc;
 public class PreProcessMain {
     
     private String image_name = "whole_o_1000.jpg"; //TODO: this is the name of the image to read
-    private String output_image_name = "smooth_1000.jpg"; //TODO: this is the name of the output image in the ~/pim/src/output_images/output_image_name.jpg
+    private String smooth_name = "smooth_image.jpg"; //TODO: this is the name of the output image in the ~/pim/src/output_images/output_image_name.jpg
+    private String clahe_name = "clahe_image.jpg"; //TODO: this is the name of the output image in the ~/pim/src/output_images/output_image_name.jpg
+    private String output_image_name = "output_image.jpg"; //TODO: this is the name of the output image in the ~/pim/src/output_images/output_image_name.jpg
     
     public void start(String pim_root_path) {
         Mat image = readImage(pim_root_path);
         
+        //apply smooth only
         Mat smooth_image = smoothImage(image);
         
-        saveImage(pim_root_path, smooth_image); 
+        saveImage(pim_root_path, smooth_name, smooth_image); 
+        
+        //apply clahe only
+        Imgproc.cvtColor(image, image, Imgproc.COLOR_RGB2GRAY);//clahe needs to be gray scale
+        
+        Mat clahe_image = new Mat();
+        CLAHE c = Imgproc.createCLAHE();
+        c.setClipLimit(4); //TODO: calibrate
+        c.apply( image, clahe_image);
+        
+        //restore to rgb
+        Imgproc.cvtColor(clahe_image, clahe_image, Imgproc.COLOR_GRAY2BGR);//cannot restore
+        
+        saveImage(pim_root_path, clahe_name, clahe_image); 
+        
+        //apply both
+        Imgproc.cvtColor(smooth_image, smooth_image, Imgproc.COLOR_RGB2GRAY);//clahe needs to be gray scale
+        c.setClipLimit(4); //TODO: calibrate
+        c.apply(smooth_image, clahe_image);
+        
+        //restore to rgb
+        Imgproc.cvtColor(clahe_image, clahe_image, Imgproc.COLOR_GRAY2BGR); //cannot restore
+        
+        saveImage(pim_root_path, output_image_name, clahe_image); 
+        
     }
     
     private Mat readImage(String pim_root_path) {
@@ -43,9 +71,9 @@ public class PreProcessMain {
         return output_image;
     }
     
-    private void saveImage(String pim_root_path, Mat image) {
-        Imgcodecs.imwrite(pim_root_path + "\\src\\output_images\\" + output_image_name, image);
-        System.out.println("saved image at = " +pim_root_path + "\\src\\output_images\\" + output_image_name);
+    private void saveImage(String pim_root_path, String output_name, Mat image) {
+        Imgcodecs.imwrite(pim_root_path + "\\src\\output_images\\" + output_name, image);
+        System.out.println("saved image at = " +pim_root_path + "\\src\\output_images\\" + output_name);
     }
 
 }
